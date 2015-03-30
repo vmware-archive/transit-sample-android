8541c06bd181bb2d2dfa49324c1ac6801733554b
Ernst Riemer <eriemer@pivotal.io>
2015-03-30 12:12:03 -0400

    EricL/TedmondH [#91415228 finish] - Added square label for the stop number - Made the stop names fit in 2 lines

--- app/src/main/java/io/pivotal/android/ttc/adapters/RoutesAdapter.java
+++ app/src/main/java/io/pivotal/android/ttc/adapters/RoutesAdapter.java
@@ -5,15 +5,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 import io.pivotal.android.ttc.R;
 import io.pivotal.android.ttc.models.Route;
 
 public class RoutesAdapter extends ArrayAdapter<Route> {
 
+    private final TextDrawable.IBuilder mTextDrawableBuilder;
+    private final ColorGenerator mGenerator;
+
     public RoutesAdapter(final Context context) {
         super(context, 0);
+
+        mTextDrawableBuilder = TextDrawable.builder()
+                .beginConfig()
+                .withBorder(4)
+                .endConfig()
+                .rect();
+
+        mGenerator = ColorGenerator.MATERIAL;
     }
 
     @Override
@@ -26,8 +41,22 @@ public class RoutesAdapter extends ArrayAdapter<Route> {
 
         final Route route = getItem(position);
 
+
+        int delimiterIndex = route.title.indexOf("-");
+        String routeNumber = "";
+        String routeName = route.title;
+        if (delimiterIndex != -1) {
+            routeNumber = route.title.substring(0, delimiterIndex);
+            routeName = route.title.substring(delimiterIndex + 1);
+        }
+
         final TextView stopView = (TextView) convertView.findViewById(R.id.route_name);
-        stopView.setText(route.title);
+        stopView.setText(routeName);
+
+        final ImageView imageView = (ImageView) convertView.findViewById(R.id.image_view);
+        TextDrawable textDrawable = mTextDrawableBuilder.build(routeNumber, mGenerator.getRandomColor());
+        imageView.setImageDrawable(textDrawable);
+
 
         return convertView;
     }
